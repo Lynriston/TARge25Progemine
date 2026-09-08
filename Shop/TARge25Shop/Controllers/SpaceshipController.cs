72064b3c11c9ac0487a4631508c25e76528f231e
Lynriston <xuliosofficial@gmail.com>
2026-09-08 10:45:01 +0300

    Lõime ühenduse SQL Serveriga

--- Shop/TARge25Shop/Controllers/SpaceshipController.cs
+++ Shop/TARge25Shop/Controllers/SpaceshipController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TARge25Shop.ApplicationServices.Services;
+using TARge25Shop.Core.Dto;
+using TARge25Shop.Core.ServiceInterface;
+using TARge25Shop.Models.Spaceship;
 
 namespace TARge25Shop.Controllers
 {
     public class SpaceshipController : Controller
     {
+        private readonly ISpaceshipServices _spaceshipServices;
+
+        public SpaceshipController(ISpaceshipServices spaceshipServices)
+        {
+            _spaceshipServices = spaceshipServices;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -13,6 +24,30 @@ namespace TARge25Shop.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(SpaceshipCreateViewModel vm)
+        {
+            var dto = new SpaceshipDto
+            {
+                Name = vm.Name,
+                ShipType = vm.ShipType,
+                Crew = vm.Crew,
+                EnginePower = vm.EnginePower
+            };
+
+            /*Nüüd kutsume teenuse välja, et luua uus kosmoselaev
+            *asünkroone tegevus ja kasutame await.*/
+            var result = await _spaceshipServices.Create(dto);
+
+            if (result == null)
+            {
+                // Handle the case when the creation fails
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Shop/TARge25Shop/Program.cs
+++ Shop/TARge25Shop/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TARge25Shop.ApplicationServices.Services;
+using TARge25Shop.Core.ServiceInterface;
+using TARge25Shop.Data;
+
 namespace TARge25Shop
 {
     public class Program
@@ -6,8 +12,15 @@ namespace TARge25Shop
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            /*Oluline on alla laadida Microsoft.EntityFrameworkCore.SqlServer NuGet
+            *paket, et saaks kasutada SQL Serverit andmebaasina.*/
+            builder.Services.AddDbContext<TARge25ShopContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<ISpaceshipServices, SpaceshipServices>();
 
             var app = builder.Build();
 
