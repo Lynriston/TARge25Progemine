11844c487d656ca8155113daefd43a51dce7fccd
Lynriston <xuliosofficial@gmail.com>
2026-09-14 15:56:27 +0300

    Indexi tegemine

--- Shop/TARge25Shop/Controllers/SpaceshipController.cs
+++ Shop/TARge25Shop/Controllers/SpaceshipController.cs
@@ -2,6 +2,7 @@
 using TARge25Shop.ApplicationServices.Services;
 using TARge25Shop.Core.Dto;
 using TARge25Shop.Core.ServiceInterface;
+using TARge25Shop.Data;
 using TARge25Shop.Models.Spaceship;
 
 namespace TARge25Shop.Controllers
@@ -9,15 +10,36 @@ namespace TARge25Shop.Controllers
     public class SpaceshipController : Controller
     {
         private readonly ISpaceshipServices _spaceshipServices;
+        private readonly TARge25ShopContext _context;
 
-        public SpaceshipController(ISpaceshipServices spaceshipServices)
+        public SpaceshipController
+            (
+            ISpaceshipServices spaceshipServices, 
+            TARge25ShopContext context
+            )
         {
             _spaceshipServices = spaceshipServices;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            //Kutsume teenuse välja, et saada kõik kosmoselaevad. See on 
+            //asünkroone tegevus ja kasutame await.
+            //constructoris tuleb välja kutsuda DbCondext, et 
+            //saaksime andmeid kätte. Seejärel kutsume teenuse välja.
+            var result = _context.Spaceships
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    ShipType = x.ShipType,
+                    CreatedAt = x.CreatedAt,
+                    Crew = x.Crew,
+                });
+
+
+            return View(result);
         }
 
         [HttpGet]
