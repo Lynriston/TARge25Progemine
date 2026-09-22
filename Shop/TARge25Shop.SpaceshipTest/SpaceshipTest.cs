8c07b74a2b78699c1e3644306ca10b7a19eb72a3
Lynriston <xuliosofficial@gmail.com>
2026-09-22 19:05:14 +0300

    Two spaceship tests

---
+++ Shop/TARge25Shop.SpaceshipTest/SpaceshipTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TARge25Shop.Core.Dto;
+using TARge25Shop.Core.ServiceInterface;
+using Xunit;
+
+namespace TARge25Shop.SpaceshipTest
+{
+    public class SpaceshipTest : TestBase
+    {
+
+        [Fact]//Fact t'histab 'ra [hte testi xUnit raamistikus
+        //ShouldNot kirjeldab öra kas test on tavaline või negatiivne
+        //AddEmptySpaceShip kirjeldab ära mida parasjagu üritatakse testialuse objektiga teha
+        //WhenResultIsReturned kirjeldab mis tingimusel tulemust kontrollitakse, peale tegevust
+        //Selles testis kontrollitakse et Kosmoselaeva lisamisel 
+        //ei tohiks saadud tulemus olla tühi. Jälgi seda sõnastusviisi
+        public async Task ShouldNot_AddEmptySpaceShip_WhenResultIsReturned()
+        {
+            //Ülesseade 
+            SpaceshipDto dto = new SpaceshipDto()
+            {
+                Name = "X AE a L 12 wfoaewfo",
+                ShipType = "Lendav taldrik",
+                Crew = 666,
+                EnginePower = 69,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+
+            //Tegutsemine
+            var result = await Svc<ISpaceshipServices>().Create(dto);
+
+            //Kontroll
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task ShouldNot_GetSpaceShipByID_WhenIDNotEqual()
+        {
+            // Ülesseade
+            Guid wrongGuid = Guid.NewGuid();
+            Guid goodGuid = Guid.Parse("a6c5a2e3-20a9-4956-915e-d289b8f5e15b");
+
+            //Tegevus
+            await Svc<ISpaceshipServices>().DetailAsync(goodGuid);
+
+            //Kontroll
+            Assert.NotEqual(wrongGuid, goodGuid);
+        }
+    }
+}
--- Shop/TARge25Shop.SpaceshipTest/TestBase.cs
+++ Shop/TARge25Shop.SpaceshipTest/TestBase.cs
@@ -23,6 +23,14 @@ namespace TARge25Shop.SpaceshipTest
             var services = new ServiceCollection();
             SetupServices(services);
         }
+        /// <summary>
+        /// Seame üles testide löbiviimiseks vajalikud teenused mujalt projektist
+        /// See meetod annab ka mölusoleva andmebaasi mida testideks kasutada,
+        /// toimib kui "program.cs"-i sisu testide jooksutamiseks, ent lühidal kujul.
+        /// </summary>
+        /// <param name="services">tühi ServiceCollection-tüübi muutuja kuhu asetame
+        /// teenused, sh ka andmebaasi</param>
+
 
         public virtual void SetupServices(ServiceCollection services)
         {
